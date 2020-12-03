849344a461050bc9b23e24d184f929022ac6f085
NanoClem <clemdecoop.pro@gmail.com>
2020-12-04 00:30:10 +0200

    upgrading code quality

--- knn.py
+++ knn.py
@@ -2,13 +2,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.utils import shuffle
 
 
 # %%
 def minkowski(a: list, b: list, p: int=1) -> float:
     """Compute the Minkowski distance between 2 data points.
-    Note that data points should have the same dimension.
+    Each data points should have the same dimension.
 
     Parameters
     -----
@@ -21,28 +20,34 @@ def minkowski(a: list, b: list, p: int=1) -> float:
         float -- Manhattan (p=1), Euclidian (p=2), ... distance between the two data points. 
     """
     dist = 0
-    for i in range(len(a)):     # we assume a and b have the same dim
-        dist += abs(a[i] - b[i])**p
+    try:
+        for i in range(len(a)):     # we assume a and b have the same dim
+            dist += abs(a[i] - b[i])**p
+        dist **= (1/p)
 
-    return dist**(1/p)
+    except IndexError as err:
+        dist = None
+        print(f"{err} : dim {len(a)} should match dim {len(b)}")
+    
+    return dist
+    
 
 
 #%%
-def knn(X_train, X_test, Y_train, k: int=3, p: int=1) -> list:
+def knn(X_train: np.ndarray, X_test: np.ndarray, Y_train: pd.DataFrame, k: int=3, p: int=1) -> list:
     """[summary]
     
     Parameters
     -----
-        X_train ([type]) -- [description]
-        Y_train ([type]) -- [description]
-        X_test ([type]) -- [description]
-        Y_test ([type]) -- [description]
-        k (int) -- [description] (default: 3)
-        p (int) -- [description] (default: 1)
+        X_train (np.ndarray) -- training data points
+        X_test (np.ndarray) -- data points on which predictions are made
+        Y_train (pd.DataFrame) -- training labels
+        k (int) -- number of neighbours to consider (default: 3)
+        p (int) -- minkowski parameter (default: 1)
 
     Returns
     -----
-        list -- [description]
+        list -- predictions about the class of each test point
     """
     from collections import Counter
     predictions = []
@@ -53,16 +58,17 @@ def knn(X_train, X_test, Y_train, k: int=3, p: int=1) -> list:
 
         for train_point in X_train:
             dist = minkowski(test_point, train_point, p=p)
-            distances.append(dist)
+            if dist: distances.append(dist)
 
-        # Getting the k-nearest neighbours
-        df_dists = pd.DataFrame(data=distances, columns=['distance'], index=Y_train.index)  # store distances in a DF
-        df_nn    = df_dists.sort_values(by='distance')[:k]                        # sort distances and keep the k closest ones
+        # Getting the k-nearest neighbours if there are ones
+        if distances:
+            df_dists = pd.DataFrame(data=distances, columns=['distance'], index=Y_train.index)  # store distances in a DF
+            df_nn    = df_dists.sort_values(by='distance')[:k]                        # sort distances and keep the k closest ones
 
-        # Classify the data point
-        counter = Counter(Y_train[df_nn.index])     # track labels of k nearest neighbours
-        predict = counter.most_common()[0][0]       # get the most common label among neighbours
-        predictions.append(predict)
+            # Classify the data point
+            counter = Counter(Y_train[df_nn.index])     # track labels of k nearest neighbours
+            predict = counter.most_common()[0][0]       # get the most common label among neighbours
+            predictions.append(predict)
 
     return predictions
 # %%
